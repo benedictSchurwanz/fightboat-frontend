287c5c9eeb240c2ee1ce4251eacc2eb45dc2163d
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2022-06-06 22:07:52 -0500

    tidy up after create-react-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="center">
-      {/* <Footer /> */}
+    <div className="app-container-div">
+      <h1>Fight Boat!</h1>
+      <h2 className="subtitle">The Board Game</h2>
     </div>
   );
 }
