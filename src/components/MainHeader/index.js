27c65b3b61c0583dbb1d768fdd6ad368c64e6e2d
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2022-06-08 18:19:30 -0500

    adjust landing page

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,4 +1,4 @@
-import PlayerName from './PlayerName.js'
+// import PlayerName from './PlayerName.js'
 
 import '../../index.css'
 
@@ -7,7 +7,7 @@ const MainHeader = () => {
 		<div> {/* container */}
 			<div className="header-bar">
 			<h1 className="title">Fight Boat!</h1>
-      <h6 className="subtitle">The Board Game</h6>
+      {/* <h6 className="subtitle">The Board Game</h6> */}
 			</div>
 		</div> /* end container */
 	);
